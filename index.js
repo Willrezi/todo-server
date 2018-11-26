b6819708867cc58244ea992abc080f6d708ac639
Wilfried LLOZA <wilfried.lloza@gmail.com>
2018-11-26 11:52:35 +0100

    add get route

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/todo");
 
+const taskModel = mongoose.model("Task", {
+  title: String,
+  isDone: {
+    default: false
+  }
+});
+
 app.get("/", function(req, res) {
-  res.json({ message: "Hello API" });
+  taskModel.find().exec(function(err, tasks) {
+    if (err) {
+      return res.status(400).json({
+        message: "an error as occured"
+      });
+    }
+    return res.status(200).json(tasks);
+  });
 });
 
 app.listen(process.env.PORT || 3000, function() {
