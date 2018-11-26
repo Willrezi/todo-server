ea8eb53319bb6f0b3cabf2d39dc1b5d46ae15527
Wilfried LLOZA <wilfried.lloza@gmail.com>
2018-11-26 12:19:28 +0100

    add create route

--- index.js
+++ index.js
@@ -10,24 +10,35 @@ app.use(bodyParser.json());
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/todo");
 
-const taskModel = mongoose.model("Task", {
-  title: String,
-  isDone: {
-    default: false
-  }
+const TaskModel = mongoose.model("Task", {
+  title: String
 });
 
-app.get("/", function(req, res) {
-  taskModel.find().exec(function(err, tasks) {
+app.get("/", (req, res) => {
+  TaskModel.find().exec((err, tasks) => {
     if (err) {
       return res.status(400).json({
-        message: "an error as occured"
+        error: err.message
       });
     }
     return res.status(200).json(tasks);
   });
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.post("/create", (req, res) => {
+  const newTask = new TaskModel(req.body);
+  console.log(req.body);
+  newTask.save((err, createdTask) => {
+    if (err) {
+      res.json({ error: err.message });
+    } else {
+      res.json(createdTask);
+    }
+  });
+});
+
+app.post("/update", (req, res) => {});
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
 });
